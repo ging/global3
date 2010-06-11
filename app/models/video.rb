49ca61dc5e4a552488e878f8773197a788cf6d83
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-06-11 13:26:35 +0200

    Video activity object

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,8 @@
 class Video < ActiveRecord::Base
   belongs_to :activity_object
-  
+
+  before_create :create_activity_object
+
+  has_many :activity_object_activities, :through => :activity_object
   
 end
