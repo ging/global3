50f13753ee9e2f1f50d6284923531b63938dfd88
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-06-15 18:09:21 +0200

    First version of home post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,26 +1,29 @@
 class Post < ActiveRecord::Base
-  attr_accessor :activity_actor
-  attr_accessor :activity_wall
-  attr_accessor :activity_role
+  attr_accessor :author
+# attr_accessor :activity_actor_id
+  attr_accessor :activity_wall_id
+  attr_accessor :activity_role_id
 
   belongs_to :activity_object
   has_many   :activity_object_activities, :through => :activity_object
 
-  validates_presence_of :activity_actor, :activity_wall, :activity_role
+  validates_presence_of :author, :activity_wall_id, :activity_role_id
 
   before_create :create_activity_object
 
-  before_validation :find_activity_contact
   before_create :create_activity
   before_update :update_activity
 
+  def validate
+    errors.add_to_base('activity.contact.not_found') unless activity_contact.present?
+  end
+
   private
 
-  def find_activity_contact
-    @activity_contact = Contact.where(:actor_from_id => activity_actor.id,
-                                      :actor_to_id   => activity_wall.id,
-                                      :role_id       => activity_role.id).first
-    errors.add_to_base('activity.contact.not_found') unless @activity_contact.present?
+  def activity_contact
+    @activity_contact = Contact.where(:actor_from_id => author.id,
+                                      :actor_to_id   => activity_wall_id,
+                                      :role_id       => activity_role_id).first
   end
 
   def create_activity
