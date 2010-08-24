ffbc497a59b5fdbef79c5f604295431d37f68245
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-08-24 11:30:52 +0200

    Add helper methods to specs

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,10 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+module ActivityTestHelper
+  def assigned_to(tie)
+    @tie = tie
+    @activity = Factory(:activity, :tie => tie)
+  end
+
+  def accessed_by(tie)
+    u = tie.sender.subject
+    @ability = Ability.new(u)
+  end
+
+  def it_should_allow(action)
+    @ability.should be_able_to(action, @activity)
+  end
+
+  def it_should_deny(action)
+    @ability.should_not be_able_to(action, @activity)
+  end
+end
+
 describe Activity do
+  include ActivityTestHelper
+
   describe "belonging to friend tie" do
     before do
-      @a = Factory(:activity, :tie => Factory(:friend_tie))
-      @ft = @a.tie
+      assigned_to(Factory(:friend_tie))
     end
 
     describe "accessed by friend" do
@@ -16,63 +37,56 @@ describe Activity do
 
     describe "accessed by other friend" do
       before do
-        t = Factory(:friend_tie, :receiver => @ft.receiver)
-        u = t.sender.subject
-        @ability = Ability.new(u)
+        accessed_by Factory(:friend_tie, :receiver => @tie.receiver)
       end
 
       it "should allow read" do
-        @ability.should be_able_to(:read, @a)
+        it_should_allow(:read)
       end
 
       it "should deny update" do
-        @ability.should_not be_able_to(:update, @a)
+        it_should_deny(:update)
       end
 
       it "should deny destroy" do
-        @ability.should_not be_able_to(:destroy, @a)
+        it_should_deny(:destroy)
       end
     end
 
     describe "accessed by friend of friend" do
       before do
-        t = Factory(:fof_tie, :receiver => @ft.receiver)
-        u = t.sender.subject
-        @ability = Ability.new(u)
+        accessed_by Factory(:fof_tie, :receiver => @tie.receiver)
       end
 
       it "should deny read" do
-        @ability.should_not be_able_to(:read, @a)
+        it_should_deny(:read)
       end
 
       it "should deny update" do
-        @ability.should_not be_able_to(:update, @a)
+        it_should_deny(:update)
       end
 
       it "should deny destroy" do
-        @ability.should_not be_able_to(:destroy, @a)
+        it_should_deny(:destroy)
       end
     end
 
     describe "accessed by other" do
       before do
-        t = Factory(:public_tie, :receiver => @ft.receiver)
-        u = t.sender.subject
-        @ability = Ability.new(u)
+        accessed_by Factory(:public_tie, :receiver => @tie.receiver)
       end
 
       it "should deny read" do
-        @ability.should_not be_able_to(:read, @a)
+        it_should_deny(:read)
       end
 
       it "should deny update" do
-        @ability.should_not be_able_to(:update, @a)
+        it_should_deny(:update)
       end
 
       it "should deny destroy" do
-        @ability.should_not be_able_to(:destroy, @a)
+        it_should_deny(:destroy)
       end
     end
-
   end
 end
