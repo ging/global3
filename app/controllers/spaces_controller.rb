992b8b6443063973cc91a1cbcd2992ccf186d648
Diego Carrera <diegocarrera2000@gmail.com>
2010-09-23 14:18:07 +0200

    changes on users and spaces

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -3,4 +3,14 @@ class SpacesController < ApplicationController
 	def index
 		
 	end
+
+  def show
+    @space = Space.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @space }
+    end
+	end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
-	
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+    end
+	end
 
 end
