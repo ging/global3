f4d96f88223647e809ea0f518ad3e5cd8c77a840
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-09-24 16:29:00 +0200

    Move activities views to social_stream

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,15 +1,2 @@
 class ActivitiesController < ApplicationController
-  # DELETE /comments/1
-  # DELETE /comments/1.xml
-  def destroy
-    @activity = Activity.find(params[:id])
-    @activity.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(activities_url) }
-      format.xml  { head :ok }
-			format.js
-    end
-	end
-
 end
