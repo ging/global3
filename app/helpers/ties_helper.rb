45735abb042e0d6cbf8717c0496c57a7a7f8bcd5
Diego Carrera <diegocarrera2000@gmail.com>
2010-10-19 18:39:41 +0200

    changes con suggestions

--- app/helpers/ties_helper.rb
+++ app/helpers/ties_helper.rb
@@ -1,11 +1,20 @@
 module TiesHelper
 
-  def suggestion_link(subject)
-
-
+  def suggestion_brief(subject)
+    "18 contacts in common"
   end
 
-  
+
+  def suggestion_link(subject)
+     link_to "+ Add as contact",
+              new_tie_path("tie[sender_id]" => current_user.actor.id,
+                        "tie[receiver_id]" => subject.actor.id,
+                        "tie[relation_name]" => "friendship_request"),
+              :class => 'boxy',
+              :title => t('ask.add_friend', :name => subject.name)
+  end
+
+
 end
 
 
