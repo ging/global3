7075f15925ba8ad12b70c12ad291f17ada9f14c5
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2010-10-25 14:54:18 +0200

    Paperclip supported logos

--- app/models/group.rb
+++
@@ -1,7 +0,0 @@
-SocialStream.require_model 'group'
-
-class Group
-  def logo
-    "spaces/#{ id % 5 + 1 }.png"
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(:version => 20100820092637) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "activity_object_id"
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
   end
 
   add_index "actors", ["activity_object_id"], :name => "fk_actors_activity_object"
