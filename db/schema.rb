5050840a8835f393b549bd7c7f1037a4b89ef64c
Diego Carrera <diegocarrera2000@gmail.com>
2011-03-11 16:41:15 +0100

    Revert "Include Mailboxer migration"
    This reverts commit abd57c6196a9380d77d901e7580e6918a9bfbb47.

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,6 @@
 # Until SocialStream database schema is stable, we will work on its migration
 require File.join(Rails.root, '..', 'social_stream/lib/generators/social_stream/templates/migration')
 
-# Load Mailboxer schema
-finder = Gem::GemPathSearcher.new
-mailboxer_spec = finder.find('mailboxer')
-mailboxer_migration =
-  finder.matching_files(mailboxer_spec,
-                       'generators/mailboxer/templates/migration.rb').first
-require mailboxer_migration
-
 ActiveRecord::Schema.define(:version => 0) do
-  CreateMailboxer.up
   CreateSocialStream.up
 end
