82d0146f7ffe3486a32edf33bdd21eaae1639941
VictorSB <v.sanchezbelmar@gmail.com>
2011-04-01 14:10:51 -0700

    Updated schema.rb for tagging

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,13 @@
 # Until SocialStream database schema is stable, we will work on its migration
 require File.join(Rails.root, '..', 'social_stream/lib/generators/social_stream/templates/migration')
 
+# Adding acts_as_taggable_on
+finder = Gem::GemPathSearcher.new
+taggable_spec = finder.find('acts-as-taggable-on')
+taggable_migration = finder.matching_files(taggable_spec,
+File.join("generators","acts_as_taggable_on","migration","templates","active_record","migration")).first
+require taggable_migration
+
 # Load Mailboxer schema
 finder = Gem::GemPathSearcher.new
 mailboxer_spec = finder.find('mailboxer')
@@ -12,4 +19,5 @@ require mailboxer_migration
 ActiveRecord::Schema.define(:version => 0) do
   CreateMailboxer.up
   CreateSocialStream.up
+  ActsAsTaggableOn.up
 end
