0aa49532274073e6ed64ea172ad3bee334dcc51e
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-05-30 12:44:20 +0200

    Change avatar thumbnail name

--- config/initializers/avatars_for_rails.rb
+++ config/initializers/avatars_for_rails.rb
@@ -3,7 +3,7 @@ AvatarsForRails.setup do |config|
   config.current_avatarable_object = :current_actor
   config.avatarable_filters = [:authenticate_user!]
   config.avatarable_styles = { :representation => "20x20>",
-                                   :tie => "30x30>",
-                                   :actor => '35x35>',
-                                   :profile => '94x94'}
+                               :contact        => "30x30>",
+                               :actor          => '35x35>',
+                               :profile        => '94x94'}
 end
