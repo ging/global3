eabdf8c3de244a3a06bfb9e9500b2237213c8674
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-06-16 10:03:23 +0200

    Add presence validations

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -1,4 +1,6 @@
 class Agenda < ActiveRecord::Base
   belongs_to :event
   has_many :sessions, :dependent => :destroy
+
+  validates_presence_of :event_id
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,6 @@
 class Session < ActiveRecord::Base
   belongs_to :agenda
   has_one :event, :through => :agenda
+
+  validates_presence_of :agenda_id
 end
