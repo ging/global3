444f5089cedc5e418475228cd695860d57566c05
VictorSB <v.sanchezbelmar@gmail.com>
2011-06-22 19:43:15 +0200

    ffmpeg development scenario added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Global::Application.configure do
   config.assets.compress = false
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  Paperclip.options[:command_path] = "/usr/local/bin"
 end
