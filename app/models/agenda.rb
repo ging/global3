105301821a3db3738dfd755c28c10ba29d71f714
Jaime Castro Montero <jaime1988@gmail.com>
2011-07-05 03:22:51 +0200

    final version

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -5,4 +5,18 @@ class Agenda < ActiveRecord::Base
   has_many :sessions, :dependent => :destroy
 
   validates_presence_of :event_id
+  
+  def to_fullcalendar_json
+    body = ""
+    
+    self.sessions.each do |session|
+      body = body + "," + session.to_fullcalendar_json
+    end
+    
+    #body = self.sessions.map{|session| session.to_fullcalendar_json}.join(",")
+    "[#{body}]"
+  end
+    
+  
 end
+
--- app/models/session.rb
+++ app/models/session.rb
@@ -5,6 +5,20 @@ class Session < ActiveRecord::Base
   has_one :event, :through => :agenda
 
   validates_presence_of :agenda_id
-  
+
   acts_as_conference_manager_session
+  def to_fullcalendar_json
+    "{
+         title: \"#{name ? sanitize_for_fullcalendar(name) : ''}\",
+         start: new Date(#{start_at.year},#{start_at.month-1},#{start_at.day},#{start_at.hour},#{start_at.min}),
+         end: new Date(#{end_at.year},#{end_at.month-1},#{end_at.day},#{end_at.hour},#{end_at.min}),
+         id: #{id},
+         allDay: false
+       }"
+  end
+
+  def sanitize_for_fullcalendar(string)
+    string.gsub("\r","").gsub("\n","<br />").gsub(/["]/, '\'')
+  end
+
 end
