5d5c5a30acdae3244078b897c5ae8dc8152f201b
Eduardo Casanova <ecasanovac@gmail.com>
2011-07-07 11:41:22 +0200

    Deleted all toolbar partials, now working with toolbar initializer

--- config/initializers/toolbar_menu.rb
+++ config/initializers/toolbar_menu.rb
@@ -20,20 +20,6 @@ ToolbarHelper.module_eval do
         {:key => :message_trash, :name => image_tag("btn/message_trash.png")+t('message.trash'), :url => conversations_path(:box => :trash)}
       ]}
 
-    #Documents if present
-    if SocialStream.activity_forms.include? :document
-      items << {:key => :resources,
-        :name => image_tag("btn/btn_resource.png",:class =>"menu_icon")+t('resource.title'),
-        :url => "#",
-        :options => {:link => {:id => "resources_menu"}},
-        :items => [
-          {:key => :resources_documents,:name => image_tag("btn/btn_documents.png")+t('document.title'),:url => documents_path},
-          {:key => :resources_pictores,:name => image_tag("btn/btn_gallery.png")+t('picture.title'),:url => pictures_path},
-          {:key => :resources_videos,:name => image_tag("btn/btn_video.png")+t('video.title'),:url => videos_path},
-          {:key => :resources_audios,:name => image_tag("btn/btn_audio.png")+t('audio.title'),:url => audios_path}
-        ]}
-    end
-
     #Contacts
     relation_items = [{:key => :invitations, :name => image_tag("btn/btn_invitation.png")+t('invitation.other'), :url => new_invitation_path}]
     current_subject.relation_customs.sort.each do |r|
@@ -47,6 +33,21 @@ ToolbarHelper.module_eval do
       :options => {:link => {:id => "contacts_menu"}},
       :items => relation_items}
 
+    #Documents if present
+    if SocialStream.activity_forms.include? :document
+      items << {:key => :resources,
+        :name => image_tag("btn/btn_resource.png",:class =>"menu_icon")+t('resource.mine'),
+        :url => "#",
+        :options => {:link => {:id => "resources_menu"}},
+        :items => [
+          {:key => :resources_conferences,:name => image_tag("btn/btn_conferences.png")+t('conference.title'),:url => videos_path},
+          {:key => :resources_documents,:name => image_tag("btn/btn_documents.png")+t('document.title'),:url => documents_path},
+          {:key => :resources_pictures,:name => image_tag("btn/btn_gallery.png")+t('picture.title'),:url => pictures_path},
+          {:key => :resources_videos,:name => image_tag("btn/btn_video.png")+t('video.title'),:url => videos_path},
+          {:key => :resources_audios,:name => image_tag("btn/btn_audio.png")+t('audio.title'),:url => audios_path}
+        ]}
+    end
+
     #Subjects
     items << {:key => :groups,
       :name => image_tag("btn/btn_group.png")+t('group.other'),
@@ -55,11 +56,17 @@ ToolbarHelper.module_eval do
       :items => [{:key => :new_group ,:name => image_tag("btn/btn_group.png")+t('group.new.action'),:url => new_group_path('group' => { '_founder' => current_subject.slug })}]
     }
 
-    return render_items items
+    render_items items
   end
 
   def profile_toolbar_menu(subject = current_subject)
     items = Array.new
+
+    #Information button
+    items << {:key => :subject_info,
+      :name => image_tag("btn/btn_edit.png")+t('menu.information'),
+      :url => [subject, :profile]
+    }
 
     if subject!=current_subject
       #Like button
@@ -71,7 +78,7 @@ ToolbarHelper.module_eval do
       if user_signed_in?
         #Relation button
         items << {:key => :subject_relation,
-          :name => image_tag("btn/btn_friend.png") + current_subject.ties_to(subject).map(&:relation_name).join(", "),
+          :name => image_tag("btn/btn_friend.png") + contact_status(subject),
           :url => edit_contact_path(current_subject.contact_to!(subject))
         }
         #Send message button
@@ -81,12 +88,26 @@ ToolbarHelper.module_eval do
         }
       end
     end
-    #Information button
-    items << {:key => :subject_info,
-      :name => image_tag("btn/btn_edit.png")+t('menu.information'),
-      :url => [subject, :profile]
-    }
     
-    return render_items items
+    #Documents if present
+    if SocialStream.activity_forms.include? :document
+      if subject == current_subject
+        resources_label = t('resource.mine')
+      else
+        resources_label = t('resource.title')        
+      end
+      items << {:key => :resources,
+        :name => image_tag("btn/btn_resource.png",:class =>"menu_icon")+resources_label,
+        :url => "#",
+        :options => {:link => {:id => "resources_menu"}},
+        :items => [
+          {:key => :resources_conferences,:name => image_tag("btn/btn_conferences.png")+t('conference.title'),:url => videos_path},
+          {:key => :resources_documents,:name => image_tag("btn/btn_documents.png")+t('document.title'),:url => documents_path},
+          {:key => :resources_pictures,:name => image_tag("btn/btn_gallery.png")+t('picture.title'),:url => pictures_path},
+          {:key => :resources_videos,:name => image_tag("btn/btn_video.png")+t('video.title'),:url => videos_path},
+          {:key => :resources_audios,:name => image_tag("btn/btn_audio.png")+t('audio.title'),:url => audios_path}
+        ]}
+    end
+    render_items items
   end
 end
