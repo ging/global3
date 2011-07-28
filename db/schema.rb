f6095c7723efadf2df6046e72ad24aecd0dbfcd7
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-07-28 15:40:10 +0200

    Event as actor, fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,28 +1,11 @@
 # Until SocialStream database schema is stable, we will work on its migration
-require 'social_stream/migration_finder'
-
-# acts-as-taggable-on
-SocialStream::MigrationFinder.new 'acts-as-taggable-on',
-                   ["generators", "acts_as_taggable_on", "migration", "templates", "active_record", "migration"]
-
-
+require 'social_stream/migrations/documents'
 
 ActiveRecord::Schema.define(:version => 0) do
-  ActsAsTaggableOnMigration.up
-
-  # Mailboxer
-  mailboxer_path = Gem::GemPathSearcher.new.find('mailboxer').full_gem_path
-  mailboxer_migration = File.join([mailboxer_path,'db', 'migrate'])
-  ActiveRecord::Migrator.migrate mailboxer_migration
-
-  # SocialStream::Base
-  ActiveRecord::Migrator.migrate File.expand_path("../../../social_stream-base/db/migrate/", __FILE__)
-
-  # SocialStream::Documents
-  ActiveRecord::Migrator.migrate File.expand_path("../../../social_stream-documents/db/migrate/", __FILE__)
+  SocialStream::Migrations::Documents.new.up
 
   create_table :events do |t|
-    t.references :activity_object
+    t.references :actor
     t.datetime :start_at
     t.datetime :end_at
     t.string   :name
