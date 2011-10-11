4b5ab5b8c6e10ae793e0bd67ec4c8347239b1d7c
Aldo Gordillo <iamchrono@gmail.com>
2011-10-11 18:01:11 +0200

    Add commented includes for Social Stream Presence, uncomment to turn it on

--- db/schema.rb
+++ db/schema.rb
@@ -1,9 +1,10 @@
 # Until SocialStream database schema is stable, we will work on its migration
 require 'social_stream/migrations/documents'
 require 'social_stream/migrations/events'
+require 'social_stream/migrations/presence'
 
 ActiveRecord::Schema.define(:version => 0) do
-  %w(Base Documents Events).each do |m|
+  %w(Base Documents Events Presence).each do |m|
     "SocialStream::Migrations::#{ m }".constantize.new.up
   end
 end
