2044e15e6f061a68b41cb96c6898bcdc1c96568c
Aldo Gordillo <iamchrono@gmail.com>
2011-11-04 12:26:09 +0100

    New fields for sstream presence initializer (Remote access and more conf options)

--- config/initializers/social_stream_presence.rb
+++ config/initializers/social_stream_presence.rb
@@ -9,16 +9,20 @@ SocialStream::Presence.setup do |config|
   config.xmpp_server_password = "G&s6GBnO)anw2Ene%K12Cb=0quj@uDmA"
   #Remote or local mode
   config.remote_xmpp_server = false
+  #Scripts path of ejabberd scripts (local or remote)
+  config.scripts_path = "/home/aldo/ejabberd-2.1.8_scripts" 
   #False to disable Social Stream Presence
   #config.enable = false
   
   #Parameters for remote mode
+  #config.ssh_domain = "trapo"
+  #config.ssh_user = "aldo"
+  #config.ssh_password= "*******"
+  
+
+  #Configure to use a Xmpp Client on Rails App
   #Username of the the Social Stream Admin sid
   config.social_stream_presence_username = "social_stream-presence"
   #Configures Social Stream Rails App Password
-  config.password = "DnVCB8G|R$VGmZ@2?5=CYS8z)NrL@LuQ&LTCUh^9B(DF4gC&sQpfbCMbaNKEdNrGYkT4L5zxM0wNWs5q3?ww(b&0d5fK87z^BmgJMMF2SKXT9pEk^UEcch!GX!Avf5GT9)j@FpHe)4RH)BK7J98u!sUmJHUN(Je6aBmn!FtZ4Ab5h8$|nsvUt3Jkq?21HOH$r0sDyJZZvGOCgFS2EKw@0wXsJRHYVRPBe&Eb!1X55e55bQ^h2AW&^R70TK0m)Fu8"
-  
-  #Parameters for local mode  
-  #Scripts path to execute local ejabberd commands
-  config.scripts_path = "/home/aldo/ejabberd-2.1.8_scripts"
+  config.password = "DnVCB8G|R$VGmZ@2?5=CYS8z)NrL@LuQ&LTCUh^9B(DF4gC&sQpfbCMbaNKEdNrGYkT4L5zxM0wNWs5q3?ww(b&0d5fK87z^BmgJMMF2SKXT9pEk^UEcch!GX!Avf5GT9)j@FpHe)4RH)BK7J98u!sUmJHUN(Je6aBmn!FtZ4Ab5h8$|nsvUt3Jkq?21HOH$r0sDyJZZvGOCgFS2EKw@0wXsJRHYVRPBe&Eb!1X55e55bQ^h2AW&^R70TK0m)Fu8" 
 end
